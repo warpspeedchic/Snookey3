03ffb7fcd183c37f729baba1067dcab07a5dd8c7
dominikrowinski <58440169+dominikrowinski@users.noreply.github.com>
2020-05-22 02:16:20 +0200

    Created files.py

---
+++ snookey3/utils/files.py
@@ -0,0 +1,23 @@
+#  Snookey3 - Unofficial streaming utility for the Reddit Public Access Network
+#  Copyright (C) 2020 warpspeedchic <https://github.com/warpspeedchic/>
+#
+#  Snookey3 is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Snookey3 is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Snookey3.  If not, see <https://www.gnu.org/licenses/>.
+
+from os import path
+
+from snookey3 import ROOT_DIR
+
+
+def get_path(*p: str) -> str:
+    return path.join(ROOT_DIR, *p)

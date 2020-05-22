7dd403b017ec5175569e3a5bfdf029e78474e115
dominikrowinski <58440169+dominikrowinski@users.noreply.github.com>
2020-05-22 02:13:45 +0200

    Created version.py

---
+++ snookey3/version.py
@@ -0,0 +1,23 @@
+#  Snookey3 - Unofficial streaming utility for the Reddit Public Access Network
+#  Copyright (C) 2020 warpspeedchic <https://github.com/warpspeedchic/>
+#
+#  Snookey3 is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Snookey3 is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Snookey3.  If not, see <https://www.gnu.org/licenses/>.
+
+__title__ = 'Snookey3'
+__description__ = 'Unofficial streaming utility for the Reddit Public Access Network'
+__url__ = 'https://github.com/warpspeedchic/Snookey3'
+__version__ = '0.1.0'
+__author__ = 'warpspeedchic'
+__copyright__ = 'Copyright (C) 2020 warpspeedchic <https://github.com/warpspeedchic/>'
+__license__ = 'GPL-3.0'

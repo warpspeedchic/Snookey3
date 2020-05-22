917d0095d14682191a84f02cc8b16cb9ec144474
dominikrowinski <58440169+dominikrowinski@users.noreply.github.com>
2020-05-22 02:14:24 +0200

    Created __main__.py

---
+++ snookey3/__main__.py
@@ -0,0 +1,24 @@
+#  Snookey3 - Unofficial streaming utility for the Reddit Public Access Network
+#  Copyright (C) 2020 warpspeedchic <https://github.com/warpspeedchic/>
+#
+#  Snookey3 is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Snookey3 is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Snookey3.  If not, see <https://www.gnu.org/licenses/>.
+
+
+def main():
+    from snookey3.version import __title__, __version__
+    print(__title__, __version__)
+
+
+if __name__ == '__main__':
+    main()
